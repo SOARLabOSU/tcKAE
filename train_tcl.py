2ea12f33bed9cb5e5090cf1b576227aebdca62d2
Ananda Chakrabarti <61379453+ananda2001@users.noreply.github.com>
2025-06-02 23:18:33 -0400

    Update train_tcl.py

--- train_tcl.py
+++ train_tcl.py
@@ -76,8 +76,6 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
             max_batch_idx = ntr // nbatch_org
                 
             for batch_idx, data_list in enumerate(train_loader):
-                
-                #print('batch_idx: ', batch_idx)
 
                 
                 if batch_idx == max_batch_idx:
@@ -90,31 +88,18 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                 out, out_back, out_lat = model(data_list[0].to(device), mode='forward') #"data_list" and "out" has shape: [steps+1][batch,1,N_tot,1]
                 
                 
-                               
-                #print('data_list length: ', len(data_list))
-                #print('data_list[0] size: ', data_list[0].size())
-                
-                #print('out_lat[0] size: ', out_lat[0].size(), 'out[0] size: ', out[0].size())
-                #loss_fwd = 0.0
-                
                 nbatch = data_list[0].size()[0]
                 
                 steps_max_fwd = min(nbatch, steps)
                 
                 for k in range(steps_max_fwd): #note that stps
                     if k == 0:
-                        #loss_fwd = criterion(out[k], data_list[k+1].to(device))
-                        #print('length of data_list : ', len(data_list), ' length of out : ',len(out))
                         loss_fwd = criterion(out[0], data_list[0+1].to(device))
                     else:
-                        #loss_fwd += criterion(out[k], data_list[k+1].to(device))
                         loss_fwd += criterion(out[k][:-k,:,:], data_list[1][k:,:,:].to(device))
 
                 
                 loss_identity = criterion(out[-1], data_list[0].to(device)) * steps
-
-                # get the size of the batch and divide by 2
-                # print('nbatch: ',nbatch)
 
 
                  
@@ -123,13 +108,10 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                     _, _, out_dae = model(data_list[0].to(device), mode='DAE_linear')
                     loss_linear_arr = torch.zeros(nbatch-1).to(device) 
                     for k in range(1,nbatch):
-                        #print('k:',k)
-                        #print('out_lat length: ', len(out_lat))
-                        # print('\n')
                         loss_linear_arr[k-1] += criterion(out_dae[-1][k:nbatch,:,:], out_dae[k-1][0:nbatch-k,:,:])
                     loss_linear = loss_linear_arr.sum()
                     
-                # This is for enforcing inf norm, for DAE (Lusch paper)
+                # This is for enforcing inf norm, for DAE
                     # Calculating the absolute difference between the tensors
                     diff1 = torch.abs(out[-1] - data_list[0].to(device))
                     diff2 = torch.abs(out[0] - data_list[1].to(device))                
@@ -150,15 +132,10 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
          
                     for kc in range(1,steps_max): 
                         for k in range(steps_tc-kc):
-                             #print('kc:',kc)
-                             #print('k:',k)
-                            # print('out length: ', len(out))
-                            # print('\n')
                             
                              if tc_opt == 1:
                                 # In latent space 
                                 loss_tc_arr[kc-1] += criterion(out_lat[k][kc:nbatch,:,:], out_lat[k+kc][0:nbatch-kc,:,:])
-                                #loss_tc_arr[kc-1] += (steps_tc-kc-k)*criterion(out_lat[k][kc:nbatch,:,:], out_lat[k+kc][0:nbatch-kc,:,:])
                              else:
                                 # In original state-space
                                 loss_tc_arr[kc-1] += criterion(out[k][kc:nbatch,:,:], out[k+kc][0:nbatch-kc,:,:])
@@ -166,23 +143,13 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                         loss_tc_arr[kc-1] = loss_tc_arr[kc-1] * (steps/(steps_tc-kc+1))  # normalize similar to other losses
                         loss_sum_norm = loss_tc_arr.sum()/(steps_max-1)
                     
-                        #print("loss_tc_arr", loss_tc_arr)
-                        #print("loss_sum_norm", loss_sum_norm)
                     if(nbatch < nbatch_org and steps_chk >= nbatch): # indicating last batch
                         steps_chk = nbatch-1
 
-                    # print('steps_max: ',steps_max)
-                    # print('nbatch: ',nbatch)
-                    # print('nbatch_org: ',nbatch_org)   
-                    # print('steps_chk: ',steps_chk)
-                    # print('loss_tc_arr size: ',loss_tc_arr.size())
-                    # print('\n')
                     if(steps_chk == 0):
                         loss_tc = loss_sum_norm
-                        #print("loss_tc: ", loss_tc)
                     else:
                         loss_tc = loss_tc_arr[steps_chk-1]  #steps_chk < steps_max
-                        #print("loss_tc: ", loss_tc)
 
                 else:
                     loss_tc = 0.0
@@ -201,11 +168,8 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                     for k in range(steps_max_bwd):
                         
                         if k == 0:
-                            #loss_bwd = criterion(out_back[k], data_list[::-1][k+1].to(device))
-                            #print('length of data_list[::-1] : ', len(data_list[::-1]), ' length of out_back : ',len(out_back), ' length of out : ',len(out))
                             loss_bwd = criterion(out_back[0], data_list[::-1][0+1].to(device))
                         else:
-                            #loss_bwd += criterion(out_back[k], data_list[::-1][k+1].to(device))
                             loss_bwd += criterion(out_back[k][k:,:,:], data_list[::-1][1][:-k,:,:].to(device))
                             
                                    
@@ -229,31 +193,10 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                             loss_consist += (torch.sum((torch.mm(Bs1, As1) - Ik)**2) + \
                                              torch.sum((torch.mm(As2, Bs2)-  Ik)**2) ) / (2.0*k)
         
-      
-                
-                # if (epoch) % 20 == 0:
-                    # #======== calculating the numerator involving loss_phy ==============
-                    # optimizer.zero_grad()        
-                    # loss_tc.backward(retain_graph = True)      # L_r in the paper  
-                    # # Calculate gradient of each parameter at each layer
-                    # #print("temporal consistency loss gradients_____________________________________________")            
-                    # grads_tc = []
-                    # param_idx = 0
-                    # for param in model.parameters():
-                        # param_idx = param_idx + 1
-                        # #print("param_idx: ", param_idx)
-                        # if(param.grad is not None):
-                            # grads_tc.append(param.grad.view(-1))            
-                    # grads_tc = torch.cat(grads_tc)
-                    # # print("grads_tc type:", type(grads_tc))
-                    # # print("grads_tc size:", grads_tc.size())
-                    # # print("grads_tc:", grads_tc)             
-                    # #print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")    
      
                 
                 if(clr == 1):
                     loss =  loss_identity + gamma_fwd * loss_fwd +  gamma_bwd * loss_bwd + gamma_con * loss_consist + gamma_tc * loss_tc + gamma_lin * loss_linear + gamma_inf * loss_inf
-                    #print("loss: ", loss)
                 else:
                     if gamma_tc == 0 and backward == 0: #DAE
                         loss = loss_identity + gamma_fwd * loss_fwd + gamma_lin * loss_linear + gamma_inf * loss_inf
@@ -271,12 +214,8 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                             loss = loss_identity + gamma_fwd * loss_fwd + gamma_lin * loss_linear + gamma_inf * loss_inf                           
                         else:
                             loss = loss_identity + gamma_fwd * loss_fwd + + gamma_lin * loss_linear + gamma_inf * loss_inf + gamma_tc * loss_tc
-                            
-                         #loss = loss_fwd + gamma_fwd * loss_identity +  gamma_bwd * loss_bwd + gamma_con * loss_consist + gamma_lin * loss_linear + gamma_inf * loss_inf               
-                    #print("loss: ", loss)
                 
                 loss_av = loss_av + loss
-                #print("loss_av: ")
                 loss_tc_av = loss_tc_av + loss_tc         
                 loss_fwd_av = loss_fwd_av + loss_fwd
                 loss_lin_av = loss_lin_av + loss_linear
@@ -352,16 +291,11 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
             if gamma_lin!=0 and gamma_inf != 0:
                 lin_loss_hist.append(loss_lin_av.data)
                 inf_loss_hist.append(loss_inf_av.data) 
-            #bwd_loss_hist.append(loss_bwd_av.data)
             if backward != 0:
                 bwd_loss_hist.append(loss_bwd_av)
                 con_loss_hist.append(loss_con_av)             
-            iden_loss_hist.append(loss_iden_av.data) 
-            #con_loss_hist.append(loss_con_av.data)               
+            iden_loss_hist.append(loss_iden_av.data)               
             epoch_loss.append(epoch)          
-            #phy_sct_hist.append(phy_sctt)  
-            #phy_scd_hist.append(phy_scdd)  
-      
             
 
             # Model evaluation at the end of each epoch 
@@ -373,19 +307,13 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                 
                 Xinput_val = Xval[0:nivs]
                 Xtarget_val = Xval[nivs+ntr:nivs+ntr+nval]
-                
-                #print("Xinput size: ",Xinput.size())
-                #print("Xtarget size: ",Xtarget.size())
 
 
                 snapshots_pred_val = []
                 snapshots_truth_val = []
 
-                #print("Xinput size :",Xinput.size()) # tensor [test_samples,1,N_1,1]
-
                 # For validation, we will use the reconstruction error in next nivs time samples
                 error = []
-                #print("nivs = ",nivs, " ntr = ",ntr, " nval = ",nval)
                 for i in range(nivs):
                     error_temp_val = []
                     init = Xinput_val[i].float().to(device)
@@ -393,7 +321,6 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                         init0 = init
                     
                     z = model.encoder(init) # embedd data in latent space
-                    #pred_steps = Xtarget.size(0)
                     m = Xtarget_val.size(2)
                     n = 1
                     
@@ -409,19 +336,10 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                             x_pred = model.decoder(z[0])
                         else:
                             x_pred = model.decoder(z) # map back to high-dimensional space
-                        #if i+j>420:
-                        # print("Epoch: ",epoch)
-                        # print("nval: ",nval)
-                        # print("Xtarget size: ",Xtarget.size())
-                        # print("i :",i)
-                        # print("j :",j)
-                        # print("i+j: ",i+j)
                         
-                        #if(j >= nivs+ntr-1-i and j <= nivs+ntr-1-i+nval-1):
                         if(j >= nivs+ntr-1-i and j <= nivs+ntr-1-i+nval-1):
                             i_tar = j - (nivs+ntr-1-i)
                             
-                            #print("i = ",i, " j = ",j, " i_tar = ",i_tar, " max{j} = ", nivs+ntr+nval-i-1)
                             target_temp_val = Xtarget_val[i_tar].reshape(m, n)
                             x_pred_temp_val = x_pred.reshape(m, n)
                             
@@ -429,8 +347,6 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                             target_temp_val = ((target_temp_val + 1) * ptpv) / 2 + minv
                             x_pred_temp_val = ((x_pred_temp_val + 1) * ptpv) / 2 + minv
 
-                            #print(x_pred_temp.device)  # should print cuda:0 or similar
-                            #print(target_temp.device)  # should print cuda:0 or similar
                             error_temp_val.append((x_pred_temp_val - target_temp_val).norm() / target_temp_val.norm())  # Here we are using PyTorch norm
 
                             if i == 0:
@@ -439,15 +355,10 @@ def train_tcl(model, train_loader, Xval, minv, ptpv, epoch_trans, gamma_tc, gamm
                             if j == pred_steps-1 - i: # save the average value at final prediction point
                                 snap_pred_val += x_pred_temp_val
                                 snap_truth_val += target_temp_val
-                    #print('error_temp length:',len(error_temp))
                     error.append(torch.stack(error_temp_val)) #error is a nivs element list where each element is nval by 1 array 
 
                 error_tensor_val = torch.stack(error)
-                #print('error_tensor size:',error_tensor.size())
-                # error_val = torch.zeros((nivs, nval), device=device)
 
-                # for i in range(nivs):
-                    # error_val[i, :] = error_tensor[i, nivs-i-1:nivs-i-1+nval]
                 error_val = error_tensor_val
                 err_mean_val = error_val.mean().item()  # Calculating mean using PyTorch
                 
